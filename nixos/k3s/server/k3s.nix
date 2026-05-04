0e0c7adc86a4261f43548b6095942cd0a395222e
sportshead <me@sportshead.dev>
2026-05-04 16:09:39 +0100

    feat: PreferNoSchedule on servers, reserve 1.5Gi for k3s

--- nixos/k3s/server/k3s.nix
+++ nixos/k3s/server/k3s.nix
@@ -2,6 +2,15 @@
   flake.nixosModules.k3s-server = {
     services.k3s = {
       role = "server";
+
+      nodeTaint = [
+        "node-role.kubernetes.io/control-plane:PreferNoSchedule"
+      ];
+
+      extraFlags = [
+        "--kubelet-arg=eviction-hard=memory.available<200Mi"
+        "--kubelet-arg=system-reserved=memory=1.5Gi"
+      ];
     };
   };
 }
