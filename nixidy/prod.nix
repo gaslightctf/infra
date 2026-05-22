97755407d0a31bfb42a3be49cb4e8ba53ecca6de
sportshead <me@sportshead.dev>
2026-05-22 12:59:31 +0100

    fix: prod berg limits

--- nixidy/prod.nix
+++ nixidy/prod.nix
@@ -8,5 +8,12 @@
       applications.berg.resources.clusters.berg-db.spec.instances = lib.mkForce 1;
 
       applications.berg.resources.horizontalPodAutoscalers.berg-api.spec.minReplicas = lib.mkForce 1;
+
+      applications.berg.helm.releases.berg.values.berg.resources = lib.mkForce {
+        requests = {
+          cpu = "500m";
+          memory = "500Mi";
+        };
+      };
     };
 }
