727d1b40e407a20e5e2ef0544f937d691a590391
sportshead <me@sportshead.dev>
2026-08-14 18:10:48 +0800

    prod: resize rayquaza

--- terranix/_prod.nix
+++ terranix/_prod.nix
@@ -1,6 +1,8 @@
 { lib, ... }:
 {
   instances.rayquaza.extraConfig = {
-    machine_type = lib.mkOverride 40 "e2-standard-2";
+    # TODO: t2d cpu limit
+    # machine_type = lib.mkOverride 40 "t2d-standard-16";
+    boot_disk.initialize_params.size = lib.mkOverride 40 100;
   };
 }
