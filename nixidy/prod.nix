c7393aa36a900ba326d14161a19b353e2a19e279
sportshead <me@sportshead.dev>
2026-08-18 17:06:06 +0800

    chore: scale down prod

---
+++ nixidy/prod.nix
@@ -0,0 +1,15 @@
+{
+  # TODO: temp overrides for mini cluster
+  flake.modules.nixidy.prod =
+    { lib, ... }:
+    {
+      applications.cilium.helm.releases.cilium.values.operator.replicas = lib.mkForce 1;
+
+      applications.berg.resources.clusters.berg-db.spec.instances = lib.mkForce 1;
+
+      applications.berg.resources.horizontalPodAutoscalers.berg-api.spec.minReplicas = lib.mkForce 1;
+      applications.berg.resources.horizontalPodAutoscalers.berg-api.spec.maxReplicas = lib.mkForce 1;
+
+      applications.berg.helm.releases.berg.values.berg.resources.requests.cpu = lib.mkForce "100m";
+    };
+}
