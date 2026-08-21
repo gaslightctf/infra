9f1b86a5fb481190ac1bbc903fd85a8393560fcd
sportshead <me@sportshead.dev>
2026-08-21 12:58:19 +0800

    chore: scale down prod

--- terranix/_prod.nix
+++ terranix/_prod.nix
@@ -1,8 +1,7 @@
 { lib, ... }:
 {
   instances.rayquaza.extraConfig = {
-    # TODO: t2d cpu limit
-    # machine_type = lib.mkOverride 40 "t2d-standard-16";
+    machine_type = lib.mkOverride 40 "t2d-standard-4";
     boot_disk.initialize_params.size = lib.mkOverride 40 100;
   };
 }
--- terranix/infra/default.nix
+++ terranix/infra/default.nix
@@ -4,12 +4,4 @@
     enable = true;
     tags = [ "server" ];
   };
-  instances.kyogre = {
-    enable = true;
-    tags = [ "server" ];
-  };
-  instances.groudon = {
-    enable = true;
-    tags = [ "server" ];
-  };
 }
